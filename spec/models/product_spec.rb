8dff17dfabe12967b912fb2e845028495ebd4cd2
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-10 20:10:15 -0400

    validate product quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Price is not a number")
     end
 
+    it 'should have a valid quantity' do
+      @category = Category.new(name: "Shrubb")
+      @product = Product.new(name: "Harbiscus", price: 800, quantity: nil, category: @category)
+      
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
     
   end
 end
