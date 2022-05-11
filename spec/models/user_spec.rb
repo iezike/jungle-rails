fa01e62da51fd99f3cba8f2ee4636b86fd9e79e7
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-11 17:28:41 -0400

    validate user saves when all field are provided

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 
 RSpec.describe User, type: :model do
-
+  describe 'Validations' do
+    it 'should save when all fields are provided' do
+      @user = User.create(name: "user", email: "user@TEST.com", password: "user_password", password_confirmation: "user_password")
+      expect(@user).to be_valid
+    end
+  end
+  
 end
