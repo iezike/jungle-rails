9bdf803f5474cfe4477dec3da0fa9f13272cd4b3
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-10 20:08:27 -0400

    validate product name

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should have a name' do
+      @category = Category.new(name: "Shrubb")
+      @product = Product.new(name: nil, price: 800, quantity: 3, category: @category)
+      
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+    
+  
+  end
+end
+
+# Price is not a number
+# Category must exist
+# Name can't be blank
+# Quantity can't be blank
+# Category can't be blank
