e892749ba8b4adacb40bac5945ebe8de161c47fc
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-11 17:32:57 -0400

    validate user cannot save when password_confirmation does not match password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe User, type: :model do
       @user = User.create(name: "user", email: "user@TEST.com", password: "user_password", password_confirmation: "user_password")
       expect(@user).to be_valid
     end
+
+    it 'should not save if password_confirmation and password fields do not match' do
+      @user = User.create(name: "user", email: "user@TEST.com", password: "user_password", password_confirmation: "user_Password")
+      expect(@user).to_not be_valid
+    end
+
   end
   
 end
