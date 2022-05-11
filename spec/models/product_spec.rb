24965d6aa39c8c918408e512dd268dbe5fb16d01
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-10 20:11:10 -0400

    validate a product category is selected

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -26,7 +26,15 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    
+    it 'should have a category selected' do
+      @category = Category.new(name: "Shrubb")
+      @product = Product.new(name: "Harbiscus", price: 800, quantity: 7, category: nil)
+      
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+
   end
 end
 
