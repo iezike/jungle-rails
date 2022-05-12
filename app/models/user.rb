897a26adda546d6bfc76dd4fc996c4699eaa2fcc
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-11 21:37:40 -0400

    add validations for user signup inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 1 }
+  has_secure_password
+
+  def authenticate_with_credentials (email, password)
+    if (self.authenticate(password))
+      return self
+    end
+    return nil
+  end
 end
