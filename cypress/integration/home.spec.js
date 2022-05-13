e1fe15238cff33426bfda3bdef3683e476e670d7
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-13 00:25:05 -0400

    refactor test to pass

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,10 +1,14 @@
 /// <reference types="cypress" />
 
-describe('example to-do app', () => {
+describe('Home page features', () => {
 
   it('should vist app homepage', () => {
     cy.visit('http://localhost:3000/')
   })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 
   it("should see 13 products on the page", () => {
     cy.get(".products article").should("have.length", 13);
